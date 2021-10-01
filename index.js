431b0188df9acd97d532c97e03fe04c4599d9771
nguyen-william93 <=>
2021-10-01 15:46:35 -0500

    testing the renderbadge function

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const inquirer = require("inquirer")
 const generateReadMe = require ("./utils/generateMarkdown")
 const fs = require ("fs");
+
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -31,98 +32,38 @@ const questions = [
     {
         type: "input",
         name: "installation",
-        message: "Provide instruction on how to use your project",
-        validate: installationInput => {
-            if (installationInput){
-                return true;
-            } else {
-                return false;
-            }
-        }
+        message: "Provide instruction on how to install your project",
     },
     {
         type: "input",
         name: "usage",
         message: "Provide any usage information",
-        validate: usageInput => {
-            if (usageInput){
-                return true;
-            } else {
-                return false;
-            }
-        }
-    },
-    {
-        type: "confirm",
-        name: "confirmContribution",
-        message: "Did you have any other colaborator?",
-        default: false
     },
     {
         type: "input",
         name: "contribution",
-        message: "Provide any contribution from any other team member.",
-        when: ({confirmContribution}) => {
-            if (confirmContribution) {
-                return true;
-            } else {
-                return false;
-            }
-        }
+        message: "Provide any contribution guidlines.",
     },
     {
         type: "input",
         name: "test",
         message: "Provide any test instruction",
-        validate: testInput => {
-            if (testInput){
-                return true;
-            } else {
-                return false
-            }
-        }
-    },
-    {
-        type: "confirm",
-        name: "confirmLicense",
-        message: "Do you have any license or need to show any license?",
-        default: true
     },
     {
         type : "checkbox",
         name: "license",
-        message: "",
-        when: ({confirmLicense}) =>{ 
-            if (confirmLicense){
-                return true;
-            } else {
-                return false;
-            }
-        }
+        message: "Choose your license.",
+        choices: ["GNU AGPLv3", "GNU GPLv3", "GNU LGPLv3", "Mozilla Public License 2.0", "Apache License 2.0", "MIT License", "Boost Software License 1.0", "The Unlicense"]
     },
     {
         type: "input",
         name: "github",
-        message: "Provide your github username",
-        validate: githubInput => {
-            if (githubInput){
-                return true;
-            } else {
-                return false;
-            }
-        }
+        message: "Provide your github username"
     },
     {
         type: "input",
         name: "email",
         message: "Provide your professional email",
-        validate: emailInput => {
-            if (emailInput){
-                return true;
-            } else {
-                return false;
-            }
-        }
     }
 ];
 
@@ -141,15 +82,16 @@ function init() {
   return inquirer.prompt(questions).then(response => {
       return generateReadMe(response);
   }).then(readme => {
+      console.log(readme);
       writeToFile ("./dist/README.md", readme, err => {
           if (err){
               console.log(err);
               return;
           }
           console.log("README was successfully created !!!");
-      })
-  })
-}
+      });
+  });
+};
 
 
 // Function call to initialize app
